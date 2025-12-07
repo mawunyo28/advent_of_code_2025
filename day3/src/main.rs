86f81ca329626aed9165a950e7d3614166cf8cc7
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2025-12-07 09:59:42 +0000

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -13,42 +13,73 @@ fn main() -> Result<(), std::io::Error> {
     let lines = buffer.lines();
 
     let sum = lines
-        .map(|line| {
+        .map(|line| -> u64 {
             let line = line.unwrap().trim().to_string();
 
-            let mut line = line.chars().collect::<Vec<char>>();
+            let line = line.chars().collect::<Vec<char>>();
 
-            print!("Line: {line:?}");
-            let mut largest: u32 = 0;
+            let numbers = line
+                .iter()
+                .map(|n| (*n).to_string().parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
 
-            line.reverse();
+            print!("{numbers:?}");
 
-            while let Some(digit1) = line.pop() {
-                line.reverse();
+            let largest = construct_largest_n_digit(numbers, 12);
 
-                // println!("\tLine: {line:?}");
-                let mut remainder = line.clone();
-
-                remainder.reverse();
-
-                while let Some(digit2) = remainder.pop() {
-                    let number = format!("{digit1}{digit2}").parse::<u32>().unwrap();
-
-                    largest = if number > largest { number } else { largest };
-                }
-
-                line.reverse();
-            }
-
-            println!("\tLargest: {largest}");
-
-            largest
+            println!("\t{largest:?}");
+            largest.unwrap()
         })
-        .collect::<Vec<u32>>()
+        .collect::<Vec<u64>>()
         .iter()
         .fold(0, |acc, val| acc + *val);
 
     println!("Sum: {sum}");
 
     Ok(())
+}
+
+fn construct_largest_n_digit(line: Vec<u32>, ndigit: u16) -> Option<u64> {
+    let mut largest: u64 = 0;
+
+    let len = line.len() as u16;
+
+    if ndigit == 0 {
+        return Some(0);
+    }
+
+    if ndigit > len {
+        return None;
+    }
+    if ndigit == len {
+        largest = line.iter().fold(0, |acc, val| acc * 10 + (*val) as u64)
+    }
+
+    if ndigit < len {
+        let mut remainder = line.clone();
+
+        let search_end = (remainder.len() - (ndigit as usize) + 1).min(remainder.len());
+
+        let first = remainder[..search_end].to_vec();
+        if let Some((index, &max_number)) = first
+            .iter()
+            .enumerate()
+            .max_by(|a, b| a.1.cmp(b.1).then(b.0.cmp(&a.0)))
+        {
+            let _ = remainder.drain(0..=index).collect::<Vec<u32>>();
+
+            if let Some(remaining_largest) = construct_largest_n_digit(remainder, ndigit - 1) {
+                largest = (max_number as u64) * 10_u64.pow((ndigit - 1) as u32) + remaining_largest;
+            }
+        }
+
+        // largest = construct_largest_n_digit(remainder, ndigit - 1).unwrap();
+
+        // println!("first: {first:?}",);
+        // println!("Largest: {largest}");
+
+        // println!("Remainder: {remainder:?}");
+    }
+
+    Some(largest)
 }
